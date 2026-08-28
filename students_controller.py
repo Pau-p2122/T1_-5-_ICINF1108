816bbe6fb9e29a5dc8f90eb81f261fd556e4ca24
Asher <agalvan2026@alu.uct.cl>
2026-08-28 16:18:03 -0400

    Estandarizacion de respuestas en controller de pets

--- students_controller.py
+++ students_controller.py
@@ -1,35 +1,49 @@
 from fastapi import APIRouter
-
+from app.pets.pets_schemas import CreatePetDto, Pet, UpdatePetDto
 from app.pets.pets_service import pets_service
-from app.students.students_schemas import CreateStudentDto, Student, UpdateStudentDto
-from app.students.students_service import students_service
+from app.students.students_schemas import StandardResponse
 
-router = APIRouter(prefix="/api/students", tags=["Students"])
+router = APIRouter(
+    prefix="/api/students/{studentId}/pets",
+    tags=["Pets"],
+)
 
+@router.get("", response_model=StandardResponse)
+def find_all(studentId: str):
+    pets = pets_service.find_all_for_student(studentId)
+    return {
+        "success": True,
+        "status": 200,
+        "message": "Mascotas obtenidas exitosamente",
+        "data": pets
+    }
 
-@router.get("")
-def find_all() -> list[Student]:
-    return students_service.find_all()
+@router.post("", status_code=201, response_model=StandardResponse)
+def create(studentId: str, body: CreatePetDto):
+    new_pet = pets_service.create(studentId, body)
+    return {
+        "success": True,
+        "status": 201,
+        "message": "Mascota creada exitosamente",
+        "data": new_pet
+    }
 
+@router.patch("/{petId}", response_model=StandardResponse)
+def update(studentId: str, petId: str, body: UpdatePetDto):
+    updated_pet = pets_service.update(studentId, petId, body)
+    return {
+        "success": True,
+        "status": 200,
+        "message": "Mascota actualizada exitosamente",
+        "data": updated_pet
+    }
 
-@router.get("/{student_id}")
-def find_by_id(student_id: str) -> Student:
-    return students_service.find_by_id(student_id)
-
-
-@router.post("", status_code=201)
-def create(body: CreateStudentDto) -> Student:
-    return students_service.create(body)
-
-
-@router.patch("/{student_id}")
-def update(student_id: str, body: UpdateStudentDto) -> Student:
-    return students_service.update(student_id, body)
-
-
-@router.delete("/{student_id}")
-def delete(student_id: str) -> Student:
-    deleted = students_service.delete(student_id)
-    pets_service.delete_all_for_student(student_id)
-
-    return deleted
+@router.delete("/{petId}", response_model=StandardResponse)
+def delete(studentId: str, petId: str):
+    deleted_pet = pets_service.delete(studentId, petId)
+    return {
+        "success": True,
+        "status": 200,
+        "message": "Mascota eliminada exitosamente",
+        "data": deleted_pet
+    }
